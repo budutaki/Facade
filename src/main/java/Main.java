0317107edb66aa21a0518b26f4ecfa8365fbe22d
Елена <budutaki@gmail.com>
2022-06-20 15:49:16 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+
+        System.out.println(bins.mult("01010101010", "0101010100101"));
+        bins.sum("0101010101001010100111", "01010101");
+
+    }
+}
